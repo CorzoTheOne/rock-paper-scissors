293245f0972277491ce00d001adeee3d746b077b
CorzoTheOne <zimonspl@live.dk>
2022-11-10 12:09:15 +0100

    Project is finished to a satisfying extent. I will continue at a later time, when i have learned more so that i can move away from it being console only.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-let score = 0;
 
+function game(number) {
+    let score = playRound(number);
+    return console.log("You won", score, "out of", number, "games!");
+}
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["Rock", "Paper", "Scissor"];
     let cpChoice = choices[Math.floor(Math.random() * choices.length)];
     return cpChoice;
 }
@@ -18,14 +21,56 @@ function gameNumbers() {
     }
 }
 function playRound(number) {
-    console.log('made it here');
-    console.log(number);
+    let wins = 0; 
     for (let x = 0; x < number; x++)
     {
-        console.log(getComputerChoice());
-        console.log(number);
+        let user = prompt("Select you action (Rock/Paper/Scissor)");
+        let computer = getComputerChoice();
+        console.log("The computer has chosen:", computer, "and you chose:", user);
 
+        if (compareActions(user, computer) == true)
+        {
+            wins++;
+        }
     }
+    return wins;
+}
+function compareActions(userAction, computerAction){
+    if (userAction == computerAction)
+    {
+        console.log("Both player chose the same. Its a tie!")
+        return false;
+    }
+    else if (userAction.toLowerCase() == "rock" && computerAction.toLowerCase() == "scissor")
+    {
+        console.log("Your rock crushes the computers scissors! You Win!")
+        return true;
+    }
+    else if (userAction.toLowerCase() == "rock" && computerAction.toLowerCase() == "paper")
+    {
+        console.log("Your rock is covered by the computers paper! You Lose!")
+        return false;
+    }
+    else if (userAction.toLowerCase() == "scissor" && computerAction.toLowerCase() == "paper")
+    {
+        console.log("Your scissor cuts the computers paper! You Win!")
+        return true;
+    }  
+    else if (userAction.toLowerCase() == "scissor" && computerAction.toLowerCase() == "rock")
+    {
+        console.log("Your scissor is crushed by the computers rock! You Lose!")
+        return false;
+    }    
+    else if (userAction.toLowerCase() == "paper" && computerAction.toLowerCase() == "rock")
+    {
+        console.log("Your paper covers the computers rock! You Win!")
+        return true;
+    } 
+    else if (userAction.toLowerCase() == "paper" && computerAction.toLowerCase() == "scissor")
+    {
+        console.log("Your paper is cut by the computers scissor! You Lose!")
+        return false;
+    }  
 }
 function gameRoundTracker() {
     score++;
